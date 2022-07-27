b55982969f1ca8310404c79d70792101edfd37cf
AnisAmeloune <lucas.boulardb@icloud.com>
2022-07-27 10:40:15 +0200

    first commit

--- coocking/src/App.js
+++ coocking/src/App.js
@@ -4,12 +4,14 @@ import Navbar from 'react-bootstrap/Navbar'
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import React from "react";
-import ListTask from './D/components-draft/ListTask';
+import ListTask from "./Draft/components-draft/ListTask"
+import HomePage from "./compounent/HomePage"
 
 const App = () => {
   return (
     <div className='App' >
-       <NavScrollExample/>
+      <HomePage/>     
+      <NavScrollExample/>
        <Navbar/>
        <ListTask/>
        <Routes>
